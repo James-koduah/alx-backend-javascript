43b3d1e0cbbdc55d7e591b818af7210fc63254b2
James Koduah <jameskoduah171@gmail.com>
2023-09-27 22:00:30 +0000

    10-loops.js

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,6 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (let elem in array) {
-    if (elem) {
-      elem = appendString + elem;
-    }
+  for (const elem of array) {
+    array[array.indexOf(elem)] = appendString + elem;
   }
 
   return array;
