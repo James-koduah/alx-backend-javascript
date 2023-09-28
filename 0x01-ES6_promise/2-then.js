a5a100d139775f3ba65ff6a45cf7b6d96cd73966
James Koduah <jameskoduah171@gmail.com>
2023-09-28 15:43:44 +0000

    2-then: so

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,10 +1,9 @@
 export default function handleResponseFromAPI(promise) {
-  promise
-    .then(() => {})
-    .then(() => {})
-    .then(() => {})
-    .catch(() => Error())
-    .finally(() => { console.log('Got a response from the API'); });
-
-    return {status: 200, body: 'success'}
+	let gg = new Promise((resolve, reject)=>{
+		if (promise) {
+			console.log("Got a response from the API")
+			resolve({ status: 200, body: 'success' });
+		}
+		else { reject(Error())}
+	})
 }
