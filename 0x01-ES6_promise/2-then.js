635b74c0297e8aa4314827cee0bf6e85d2f79900
James Koduah <jameskoduah171@gmail.com>
2023-09-28 15:55:56 +0000

    2-then: lint fix

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,10 +1,9 @@
 export default function handleResponseFromAPI(promise) {
-	let gg = new Promise((resolve, reject)=>{
-		if (promise) {
-			console.log("Got a response from the API")
-			resolve({ status: 200, body: 'success' });
-		}
-		else { reject(Error())}
-	})
-	return gg
+  const gg = new Promise((resolve, reject) => {
+    if (promise) {
+      console.log('Got a response from the API');
+      resolve({ status: 200, body: 'success' });
+    } else { reject(Error()); }
+  });
+  return gg;
 }
