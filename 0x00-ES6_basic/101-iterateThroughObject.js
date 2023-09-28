2ab3d168d3da8fbab6d0f92fff2fdee1dd38b3c2
James Koduah <jameskoduah171@gmail.com>
2023-09-28 11:52:12 +0000

    101-iterateThroughObject.js

---
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,13 @@
+export default function iterateThroughObject(reportWithIterator) {
+  let string = '';
+  let i = false;
+  for (const elem of reportWithIterator) {
+    if (!i) {
+      string += `${elem} `;
+      i = true;
+    } else {
+      string += `| ${elem} `;
+    }
+  }
+  return string;
+}
