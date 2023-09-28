c31059492c44ebb51f8db7e1244878a0a0770944
James Koduah <jameskoduah171@gmail.com>
2023-09-28 15:29:58 +0000

    2-then.js: used finally block

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -2,9 +2,7 @@ export default function handleResponseFromAPI(promise) {
   promise
     .then(() => {})
     .then(() => {})
-    .then(() => {
-      console.log('Got a response form the API');
-      return { status: 200, body: 'success' };
-    })
-    .catch(() => Error());
+    .then(() => {})
+    .catch(() => Error())
+    .finally(() => { console.log('Got a response from the API'); });
 }
