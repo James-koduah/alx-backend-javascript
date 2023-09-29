f622fb3203b73ffed012a044fefd658fd7edae15
James Koduah <jameskoduah171@gmail.com>
2023-09-29 18:32:12 +0000

    9-try.js

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,11 @@
+export default function guardrail(mathFunction) {
+  const queue = [];
+  try {
+    const res = mathFunction();
+    queue.push(res);
+  } catch (e) {
+    queue.push(`Error: ${e.message}`);
+  }
+  queue.push('Guardrail was processed');
+  return queue;
+}
