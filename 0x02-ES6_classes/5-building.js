4ee34b4c5ce80c4ad3e5d2c38a15d287ab6aeb0c
James Koduah <jameskoduah171@gmail.com>
2023-09-30 21:00:18 +0000

    5-building.js

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,6 +1,6 @@
 export default class Building {
   constructor(sqft) {
-    if (this.evacuationWarningMessage === undefined) {
+    if (this.evacuationWarningMessage === undefined && new.target !== Building) {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
     if (typeof sqft !== 'number') {
