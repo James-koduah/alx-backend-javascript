80449b4315f07e655ab3b21d8c071a478115a3b9
James Koduah <jameskoduah171@gmail.com>
2023-10-03 12:35:02 +0000

    8-clean_set.js: second(typeof startString)

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,5 @@
 export default function cleanSet(set, startString) {
-  if (startString === '') return '';
+  if (startString === '' || typeof startString !== 'string') return '';
   const final = [];
   for (const item of set) {
     if (item.startsWith(startString)) {
