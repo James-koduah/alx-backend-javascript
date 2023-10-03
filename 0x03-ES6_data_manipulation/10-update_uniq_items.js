d88fae27f1da137a01072638172a3ac1398f04ac
James Koduah <jameskoduah171@gmail.com>
2023-10-03 12:50:02 +0000

    10-update_uniq_items.js: second(if argument is not a map)

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,4 +1,5 @@
 export default function updateUniqueItems(arg) {
+  if (!(arg instanceof Map)) throw new Error('Cannot process');
   const update = arg;
   for (const [key, value] of arg) {
     if (value === 1) update.set(key, 100);
