4488f43e358a02edde4a26abcc4b0642a1cfd64f
James Koduah <jameskoduah171@gmail.com>
2023-10-03 12:40:37 +0000

    9-groceries_list.js: first

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,10 @@
+export default function groceriesList() {
+  const mapL = new Map();
+  mapL.set('Apples', 10);
+  mapL.set('Tomatoes', 10);
+  mapL.set('Pasta', 1);
+  mapL.set('Rice', 1);
+  mapL.set('Banana', 5);
+
+  return mapL;
+}
