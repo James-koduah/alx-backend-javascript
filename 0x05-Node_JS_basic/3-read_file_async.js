983b36d9551e0fa125b5ffc3ec2a3e58c5d75d4c
james-koduah <jameskoduah171@gmail.com>
2024-01-30 13:06:02 +0000

    task 3: 3-read_file_async: first and other files

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,30 @@
+const fs = require('fs/promises');
+
+async function countStudents(path) {
+  let data;
+  try {
+    data = await fs.readFile(path, 'utf8');
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+
+  const rows = data.split('\n');
+  const cs = [];
+  const swe = [];
+  let studentNumber = 0;
+  for (let i = 1; i < rows.length; i += 1) {
+    const row = rows[i];
+    const details = row.split(',');
+    if (details.length === 4) {
+      if (details[3] === 'CS') cs.push(details[0]);
+      if (details[3] === 'SWE') swe.push(details[0]);
+      studentNumber += 1;
+    }
+  }
+  console.log(`Number of students: ${studentNumber}`);
+  console.log(`Number of students in CS: ${cs.length}. List: ${cs.join(', ')}`);
+  console.log(`Number of students in SWE: ${swe.length}. List: ${swe.join(', ')}`);
+  return 1;
+}
+
+module.exports = countStudents;
