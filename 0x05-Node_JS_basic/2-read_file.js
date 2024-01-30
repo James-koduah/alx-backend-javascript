aa6621897acd29139d091d5a4e54f294af231899
james-koduah <jameskoduah171@gmail.com>
2024-01-30 11:55:30 +0000

    task 2: 2-read_file.js: first

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,27 @@
+const fs = require('node:fs');
+
+function countStudents(path) {
+  fs.readFile(path, 'utf8', (err, data) => {
+    if (err) {
+      throw new Error('Cannot load the database');
+    }
+    const rows = data.split('\n');
+    const cs = [];
+    const swe = [];
+    let studentNumber = 0;
+    for (let i = 1; i < rows.length; i += 1) {
+      const row = rows[i];
+      const details = row.split(',');
+      if (details.length === 4) {
+        if (details[3] === 'CS') cs.push(details[0]);
+        if (details[3] === 'SWE') swe.push(details[0]);
+        studentNumber += 1;
+      }
+    }
+    console.log(`Number of students: ${studentNumber}`);
+    console.log(`Number of students in CS: ${cs.length}. List: ${cs.join(', ')}`);
+    console.log(`Number of students in SWE: ${swe.length}. List: ${swe.join(', ')}`);
+  });
+}
+
+module.exports = countStudents;
