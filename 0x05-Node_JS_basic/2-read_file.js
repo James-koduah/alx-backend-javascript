f46e377549993c3f4cb3e831d64b8868e4e24a4d
james-koduah <jameskoduah171@gmail.com>
2024-01-30 12:06:56 +0000

    task 2: 2-read_file.js: read file with fs.readFileSync

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,27 +1,26 @@
 const fs = require('fs');
 
 function countStudents(path) {
-  fs.readFile(path, 'utf8', (err, data) => {
-    if (err) {
-      throw new Error('Cannot load the database');
+  if (!fs.existsSync(path)) {
+    throw new Error('Cannot load the database');
+  }
+  const data = fs.readFileSync(path, 'utf8');
+  const rows = data.split('\n');
+  const cs = [];
+  const swe = [];
+  let studentNumber = 0;
+  for (let i = 1; i < rows.length; i += 1) {
+    const row = rows[i];
+    const details = row.split(',');
+    if (details.length === 4) {
+      if (details[3] === 'CS') cs.push(details[0]);
+      if (details[3] === 'SWE') swe.push(details[0]);
+      studentNumber += 1;
     }
-    const rows = data.split('\n');
-    const cs = [];
-    const swe = [];
-    let studentNumber = 0;
-    for (let i = 1; i < rows.length; i += 1) {
-      const row = rows[i];
-      const details = row.split(',');
-      if (details.length === 4) {
-        if (details[3] === 'CS') cs.push(details[0]);
-        if (details[3] === 'SWE') swe.push(details[0]);
-        studentNumber += 1;
-      }
-    }
-    console.log(`Number of students: ${studentNumber}`);
-    console.log(`Number of students in CS: ${cs.length}. List: ${cs.join(', ')}`);
-    console.log(`Number of students in SWE: ${swe.length}. List: ${swe.join(', ')}`);
-  });
+  }
+  console.log(`Number of students: ${studentNumber}`);
+  console.log(`Number of students in CS: ${cs.length}. List: ${cs.join(', ')}`);
+  console.log(`Number of students in SWE: ${swe.length}. List: ${swe.join(', ')}`);
 }
 
 module.exports = countStudents;
