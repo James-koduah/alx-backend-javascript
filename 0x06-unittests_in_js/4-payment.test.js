6d2f29fb93e5582c61e6bdef8253fc3369d9c1bc
james-koduah <jameskoduah171@gmail.com>
2024-02-02 17:24:00 +0000

    task 4: errors fixed

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -9,11 +9,11 @@ describe('sendPaymentRequestToApi', ()=>{
   it('check if the utils function', ()=>{
     const console_spy = sinon.spy(console);
     const calculateNumber = sinon.stub(utils, 'calculateNumber');
-    calculateNumber.returns(20);
+    calculateNumber.returns(10);
     sendPayment(100, 20)
     expect(calculateNumber.calledWith('SUM', 100, 20)).to.be.true;
-    expect(spyConsole.log.calledWith('The total is: 10')).to.be.true;
+    expect(console_spy.log.calledWith('The total is: 10')).to.be.true;
     calculateNumber.restore()
-    console_spy.restore()
+    console_spy.log.restore()
   })
 })
