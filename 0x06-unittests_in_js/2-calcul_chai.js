5306bdab7928dd6fab7ba0781b026cf905fce4a9
james-koduah <jameskoduah171@gmail.com>
2024-02-02 15:32:34 +0000

    task 2: installed chai: first

---
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -0,0 +1,16 @@
+function calculateNumber(type, a, b){
+  aa = Math.round(a)
+  bb = Math.round(b)
+  if (type === 'SUM'){
+    return aa + bb
+  }
+  if (type === 'SUBTRACT'){
+    return aa - bb
+  }
+  if (type === 'DIVIDE'){
+    if (bb === 0) return 'Error'
+    return aa / bb
+  }
+}
+
+module.exports = calculateNumber
---
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -0,0 +1,31 @@
+const chai = require("chai");
+const calculateNumber = require('./2-calcul.js')
+
+const expect = chai.expect;
+
+describe('calculateNumber', function() {
+  describe('testing Sum', ()=>{
+    it('checks if it sums correctly', ()=>{
+      let result = calculateNumber('SUM', 1.4, 4.5)
+      expect(result).to.equal(6)
+    })
+  })
+  describe('testing Subtract', ()=>{
+    it('checks if it subtracts correctly', ()=>{
+      let result = calculateNumber('SUBTRACT', 1.4, 4.5)
+      expect(result).to.equal(-4)
+    })
+  })
+  describe('testing Divide', ()=>{
+    it('checks if it divides correctly', ()=>{
+      let result = calculateNumber('DIVIDE', 1.4, 4.5)
+      expect(result).to.equal(0.2)
+    })
+  })
+  describe('testing Divide by Zero', ()=>{
+    it('checks if it returns error', ()=>{
+      let result = calculateNumber('DIVIDE', 1.4, 0)
+      expect(result).to.equal('Error')
+    })
+  })
+})
